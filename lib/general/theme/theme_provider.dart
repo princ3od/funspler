ad3e5d96c903a1512910c7131d99abd0759a6246
Trong <princ3od@gmail.com>
2021-09-23 10:05:18 +0700

    Init project structure

---
+++ lib/general/theme/theme_provider.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  late bool isLightTheme;
+
+  ThemeProvider({required this.isLightTheme});
+
+  getCurrentStatusNavigationBarColor() {
+    if (isLightTheme) {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Color(0xFFFFFFFF),
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ));
+    } else {
+      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarBrightness: Brightness.dark,
+        statusBarIconBrightness: Brightness.light,
+        systemNavigationBarColor: Color(0xFF26242e),
+        systemNavigationBarIconBrightness: Brightness.light,
+      ));
+    }
+  }
+
+  toggleThemeData() async {
+    final settings = await Hive.openBox('settings');
+    settings.put('isLightTheme', !isLightTheme);
+    isLightTheme = !isLightTheme;
+    getCurrentStatusNavigationBarColor();
+    notifyListeners();
+  }
+
+  ThemeData globalTheme() {
+    return ThemeData(
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      primarySwatch: isLightTheme ? Colors.grey : Colors.grey,
+      primaryColor: isLightTheme ? Colors.white : Color(0xFF1E1F28),
+      brightness: isLightTheme ? Brightness.light : Brightness.dark,
+      backgroundColor: isLightTheme ? Color(0xFFFFFFFF) : Color(0xFF26242e),
+      scaffoldBackgroundColor:
+          isLightTheme ? Color(0xFFFFFFFF) : Color(0xFF26242e),
+    );
+  }
+}
